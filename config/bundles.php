09856a12583bdc08f10a1496ff98c09e855f600e
mdieudonne <michael.dieudonne@outlook.com>
2021-02-03 13:58:40 +0100

    chore: add debug and validator config

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
